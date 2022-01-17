de9ddc5c647743b0ab46a3a4d360e8be594bb277
Liza Lolli <lizalollitorres@gmail.com>
2022-01-16 22:27:30 -0300

    tweets + likes + delete

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {auth, LoginGoogle, logout} from './firebase'
+
+const LoginGoogle = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default Google
