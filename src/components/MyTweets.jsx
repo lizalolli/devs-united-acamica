f2dfe51f35a3cc6089d5c0cfa5a683036b005714
Liza Lolli <lizalollitorres@gmail.com>
2022-02-13 15:57:59 -0300

    Add files via upload

--- src/components/MyTweets.jsx
+++ src/components/MyTweets.jsx
@@ -20,7 +20,7 @@ const MyTweets = () => {
                                     <h4 className="tweet-autor">{tweet.user}</h4>
                                     <span onClick={() => deleteTweet(tweet.id)} className="delete">{user.uid === tweet.uid && (<img src="./images/delete.svg" alt=""/> )}</span>
                                 </div>
-                                <p>{tweet.tweet}</p>
+                                <p className="text-break">{tweet.tweet}</p>
                                 {showLike(tweet.likedBy, tweet.id, tweet.likes)}
                             </div>
                         </div>
